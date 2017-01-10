403f86dea9d5585a3d6bc79971cebdc7b621e0aa
Brian McEvoy <brian@doubleencore.com>
2017-01-10 12:13:32 -0700

    Create Framework target

---
+++ MaterialControls/MaterialControls-Dynamic/MaterialControls-Dynamic.h
@@ -0,0 +1,13 @@
+//
+//  MaterialControls-Dynamic.h
+//  MaterialControls-Dynamic
+//
+//  Created by Brian McEvoy on 1/10/17.
+//  Copyright © 2017 FPT Software. All rights reserved.
+//
+
+
+
+// In this header, you should import all the public headers of your framework using statements like #import <MaterialControls_Dynamic/PublicHeader.h>
+
+
